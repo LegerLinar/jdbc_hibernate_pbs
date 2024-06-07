78a60ba10b7874958f499596f34be28c91f0d959
LegerLinar <pavelbesedin97@mail.ru>
2024-06-08 00:01:21 +0700

    DB connected

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Util util = new Util();
+        System.out.println(util.getConnection().isClosed());
         // реализуйте алгоритм здесь
     }
 }
